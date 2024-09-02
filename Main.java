995f6bdbcd07ec87244ab6efc67b3fa94c1bd8d6
Wahyu Nur Hidayat <wh664748@gmail.com>
2024-09-02 22:36:59 +0700

    Up tugas pbow2

---
+++ Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author hp
+ */
+public class Main {
+    
+   public static void main(String[] args) {
+        System.out.println("Nama   : Wahyu Nur Hidayat");
+        System.out.println("NPM    : 2320506027");
+        System.out.println("Alamat : Purworejo");
+        System.out.println("No_HP  : 085292319655");
+    }
+}
+
